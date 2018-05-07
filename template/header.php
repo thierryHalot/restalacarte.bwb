e344fa97b5bbbd1564c70ed67e53935a46b9aca6
Thierry <thierry.halot@laposte.net>
2018-05-07 11:59:12 +0200

    import bootstrap et feuille de style (operationel)

--- template/header.php
+++ template/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <meta charset="UTF-8">
-
+    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" media="all"/>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" media="all"/>
+    
 </head>
 <body>
