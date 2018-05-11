a40cba339589586f21519f1b50300d667669e3cc
Thierry <thierry.halot@laposte.net>
2018-05-11 11:31:24 +0200

    traitement des messages du livre d'or

---
+++ script/livreDor.php
@@ -0,0 +1,13 @@
+<?php
+$listeDeMessages = array();
+
+$message = array(
+
+        "nom" => $_POST['nomLivreDor'],
+        "prenom" => $_POST['prenomLivreDor'],
+        'message' => $_POST['messageLivreDor'],
+
+
+);
+
+
