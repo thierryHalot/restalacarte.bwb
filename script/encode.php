0c42e255d89abecd3e277cd7be70275d3d5b230e
Thierry <thierry.halot@laposte.net>
2018-05-13 21:01:07 +0200

    script permetant de stocker les messages du livre d'or dans un fichier json

--- script/encode.php
+++ script/encode.php
@@ -1,5 +1,5 @@
 <?php
-$listeDeMessages = array();
+
 
 //je definis un message
 $message = array(
@@ -11,28 +11,62 @@ $message = array(
 
 );
 
+//le serveur envoi un requete de type get vers le fichier message.jon
+//et recupere le fichier sous forme de chainde caractère
+//l'argument true sert a rechercher le fichier dans le chemin d'inclusion c'est a dire dans le chemin spécifier en 1er argument
 
-
-//je pousse mon message dans ma listes de message
-array_push($listeDeMessages,$message);
-
-
-
-
-//j'encode mon message en json
-$messageEncoder = json_encode($listeDeMessages);
-
-//j'insere ma liste de message dans mon fichier ,le fichier est ecraser
-
-
-    $ecritureFichier = file_put_contents('../data/message.json',$messageEncoder);
+    $contenuFichier = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/data/message.json',true);
 
 
 
+    //strlen retoune le nombre de caractere contenu dans mon ficchier
+ //si mon fichier est vide
+    if(strlen($contenuFichier)=== 0){
+
+     //je crée un tableau contenant mes messages
+        $listeDeMessages = array();
+
+        //j'insere le message que l'uttilisateur a poster dans ma liste de message
+        array_push($listeDeMessages, $message);
+
+
+        //j'encode mon message en json pour l'envoi
+        $messageEncoder = json_encode($listeDeMessages);
+
+        //le serveur fait une requete de type post vers le fichier
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].'/data/message.json', $messageEncoder);
+
+//sinon,mon tableau liste de message étant déja crée alors...
+    }else{
 
 
 
 
+        //je decode mon tableau de json, nouvelle requette de type get du serveur vers mon fichier
+
+        $tableauJson = json_decode($contenuFichier);
+
+
+
+        //je rajoute le message dans le tableau que j'ai recuperer de mon fichier
+
+        array_push($tableauJson, $message);
+
+
+
+        //je réencode mon tableau en json pour l'envoi
+
+        $messageEncoder = json_encode($tableauJson);
+
+
+
+        //le serveur post le contenus vers mon fichier avec les données actualisées
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].'/data/message.json', $messageEncoder);
+
+
+    }
+//je redirige l'uttilisateur vers le livre d'or (php(point de vue serveur) crée le contenue du livre d'or et l'envoie sur l'index,le navigateur affiche le contenus !!!!)
 header('Location: http://www.php-decouverte.bwb/?chemin=livre_d_or');
 
 
