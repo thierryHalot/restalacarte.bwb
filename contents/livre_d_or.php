ad5ea38776147cbbd5a4500baf98701806adbac1
Thierry <thierry.halot@laposte.net>
2018-05-13 21:03:32 +0200

    appelle de fonction pour la vue

--- contents/livre_d_or.php
+++ contents/livre_d_or.php
@@ -1,41 +1,7 @@
 <h1>livre d'or</h1>
-<div class="row">
 
 <?php
-if (isset($_SESSION['pseudo'])) {
-
-    ?>
- <div class="col-5">
-    <form method="post" action="../script/encode.php" class="forLivreDor">
-        <label for="nomLivreDor">Votre nom</label> : <input type="text" name="nomLivreDor" id="nomLivreDor" class="inpLivreDor"/><br/>
-        <label for="prenomLivreDor">Votre prenom</label> : <input type="text" name="prenomLivreDor" id="prenomLivreDor"class="inpLivreDor"/><br/>
-        <p>
-            <label for="messageLivreDor">message : </label><br/>
-            <textarea name="messageLivreDor" id="messageLivreDor" class="textaLivreDor"></textarea>
-        </p>
-        <input type="submit" value="Envoyer" class="bouton"/>
-    </form>
- </div>
-    <?php
-};
-include './script/decode.php';
+require_once "script/fonction.php";
+echo getFormLivreDor();
 ?>
-    <div class="col-5 test">
-        <?php
 
-        foreach ($fichierDecoder as $key => $value){
-
-
-
-            foreach ($value as $key1 => $value1){
-
-
-                echo $key1." : ".$value1."<br>";
-
-
-            }
-
-
-        }?>
-    </div>
-</div>
--- template/content.php
+++ template/content.php
@@ -2,14 +2,8 @@
 
     <?php
 
-//si ma clé 'chemin' existe dans l'url
-    if(isset($_GET['chemin'])){
-
-//j'insere le contenu de la page qui m'interresse en rajoutant l'extension
-    include "contents/".$_GET['chemin'].".php";
-
-
-    }
+    require_once "script/fonction.php";
+    echo getNavContent();
     ?>
 
 
