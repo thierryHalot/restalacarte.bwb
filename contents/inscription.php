d2a455610b15be8ce64f94b4fdaecb90d778b5d6
Thierry <thierry.halot@laposte.net>
2018-05-13 22:24:42 +0200

    rajout d'un formulaire d'inscription basic

---
+++ contents/inscription.php
@@ -0,0 +1,9 @@
+<div class="">
+    <form method="post" action="../script/inscription.php" class="forLivreDor">
+
+        <label for="pseudoInscription">Pseudo</label> : <input type="text" name="pseudoInscription" id="pseudoInscription" class="inpLivreDor"/><br/>
+        <label for="mdpInscription">Mot de passe</label> : <input type="text" name="mdpInscription" id="mdpInscription"class="inpLivreDor"/><br/>
+
+        <input type="submit" value="s'inscrire" class="bouton"/>
+    </form>
+</div>
