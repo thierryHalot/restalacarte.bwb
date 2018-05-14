cc3475d5dddcb078a0ea4d4ee4fada00142b141e
Thierry <thierry.halot@laposte.net>
2018-05-14 11:48:37 +0200

    connexion après vérification dans la bdd

--- script/connexion.php
+++ script/connexion.php
@@ -1,6 +1,26 @@
 <?php
 session_start();
 
-$_SESSION['pseudo'] = $_POST['pseudo'];
-$_SESSION['mdp'] = $_POST['mdp'];
-header('Location: http://www.php-decouverte.bwb/?chemin=livre_d_or');
+include $_SERVER['DOCUMENT_ROOT'].'/script/decodeUsers.php';
+
+
+    foreach ($fichierDecoderUsers as  $value) {
+
+
+
+
+                if ($_POST['pseudo'] === $value['Pseudo'] && $_POST['mdp'] === $value['mdp'] ) {
+
+                        $_SESSION['pseudo'] = $_POST['pseudo'];
+                        $_SESSION['mdp'] = $_POST['mdp'];
+                        var_dump($_POST['pseudo']);
+                    }
+
+            //echo $key1." : ".$value1."<br>";
+
+    }
+
+//$_SESSION['pseudo'] = $_POST['pseudo'];
+//$_SESSION['mdp'] = $_POST['mdp'];
+//header('Location: http://www.php-decouverte.bwb/?chemin=livre_d_or');
+?>
