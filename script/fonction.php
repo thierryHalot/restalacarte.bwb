f0432beba42bc7184872dbf4222bdc7bea010f63
Thierry <thierry.halot@laposte.net>
2018-05-17 12:11:55 +0200

    rajout fonction decodejsonfile

--- script/fonction.php
+++ script/fonction.php
@@ -91,10 +91,7 @@ function getNav(){
 </nav>
   <?php
 }
-?>
 
-
-<?php
 //fonction qui permet de d'afficher le bon contenu correspondant au ancre de ma barre nav
 function getNavContent(){
 
@@ -109,6 +106,19 @@ function getNavContent(){
 
 }
 
+//fonction qui prend en argument le chemin du fichier a decoder a partir du document root
+//et qui permet de le retourner sous forme de tableau associatif
+function decodeJsonFile ($cheminFichier){
+
+    $recuperationFichier = file_get_contents($_SERVER['DOCUMENT_ROOT'].$cheminFichier,true);
+
+    $fichierDecoder = json_decode($recuperationFichier);
+
+    return $fichierDecoder;
+
+
+};
+//fonction pour l'affichage du formulaire du livre d'or
 function getFormLivreDor (){
 
 
@@ -131,7 +141,9 @@ function getFormLivreDor (){
         </div>
         <?php
     };
-    include $_SERVER['DOCUMENT_ROOT'].'/script/decode.php';
+
+    $fichierDecoder = decodeJsonFile('/data/message.json');
+
     ?>
     <div class="col-5 test">
         <?php
@@ -159,6 +171,79 @@ function getFormLivreDor (){
 }
 
 //fonction qui permet de rajouter un nouvelle uttilisateur dans la bdd
+
+function encodeMsg(){
+
+//je definis un message
+    $message = array(
+
+        "nom" => $_POST['nomLivreDor'],
+        "prenom" => $_POST['prenomLivreDor'],
+        'message' => $_POST['messageLivreDor'],
+
+
+    );
+
+//le serveur envoi un requete de type get vers le fichier message.jon
+//et recupere le fichier sous forme de chainde caractère
+//l'argument true sert a rechercher le fichier dans le chemin d'inclusion c'est a dire dans le chemin spécifier en 1er argument
+
+    $contenuFichier = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/data/message.json',true);
+
+
+
+    //strlen retoune le nombre de caractere contenu dans mon ficchier
+    //si mon fichier est vide
+    if(strlen($contenuFichier)=== 0){
+
+        //je crée un tableau contenant mes messages
+        $listeDeMessages = array();
+
+        //j'insere le message que l'uttilisateur a poster dans ma liste de message
+        array_push($listeDeMessages, $message);
+
+
+        //j'encode mon message en json pour l'envoi
+        $messageEncoder = json_encode($listeDeMessages);
+
+        //le serveur fait une requete de type post vers le fichier
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].'/data/message.json', $messageEncoder);
+
+//sinon,mon tableau liste de message étant déja crée alors...
+    }else{
+
+
+
+
+        //je decode mon tableau de json, nouvelle requette de type get du serveur vers mon fichier
+
+        $tableauJson = json_decode($contenuFichier);
+
+
+
+        //je rajoute le message dans le tableau que j'ai recuperer de mon fichier
+
+        array_push($tableauJson, $message);
+
+
+
+        //je réencode mon tableau en json pour l'envoi
+
+        $messageEncoder = json_encode($tableauJson);
+
+
+
+        //le serveur post le contenus vers mon fichier avec les données actualisées
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].'/data/message.json', $messageEncoder);
+
+
+    }
+//je redirige l'uttilisateur vers le livre d'or (php(point de vue serveur) crée le contenue du livre d'or et l'envoie sur l'index,le navigateur affiche le contenus !!!!)
+    header('Location: http://www.restalacarte.bwb/?chemin=livre_d_or');
+
+}
+
 function addUser($user){
 
 
@@ -214,7 +299,7 @@ function addUser($user){
 //function qui permet de vérifier si le nom d'uttilisateur et le mot de passe taper a la connexion correspond avec la bdd
 function getUser($username,$mdp){
 
-    include $_SERVER['DOCUMENT_ROOT'].'/script/decodeUsers.php';
+    $fichierDecoderUsers = decodeJsonFile('/data/users.json');
 
     foreach ($fichierDecoderUsers as  $value) {
 
@@ -235,3 +320,4 @@ function getUser($username,$mdp){
 
 
 }
+
